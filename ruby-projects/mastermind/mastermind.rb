b8bccc13f43e47ace83002ead220ff3b851bbe9a
TJancenkovas <t.jancenkovas@gmail.com>
2023-06-09 12:20:19 +0300

    Added fully added cpu logic     Need to verify it     Fix interaction with game class

--- ruby-projects/mastermind/mastermind.rb
+++ ruby-projects/mastermind/mastermind.rb
@@ -1,6 +1,56 @@
 require 'pry'
 
+module AccuracyFinder
+
+  def get_accuracy(guess, code)
+    temp_code = code.clone
+    temp_guess = guess.clone
+    accuracy = []
+
+    get_exact(temp_guess, temp_code).times { accuracy.append('E') }
+    get_partial(temp_guess, temp_code).times { accuracy.append('P') }
+
+    accuracy
+  end
+
+  def print_accuracy(guess, code)
+    accuracy = get_accuracy(guess, code).each do |result|
+      print "#{result} "
+    end
+    puts
+    accuracy
+  end
+
+  def get_exact(guess, code)
+    exact = 0
+    guess.each_with_index do |slot, index|
+      next unless slot == code[index]
+
+      exact += 1
+      guess[index] = '-'
+      code[index] = '-'
+    end
+    exact
+  end
+
+  def get_partial(guess, code)
+    partial = 0
+    guess.each_with_index do |slot, index|
+      next unless slot != '-' && code.include?(slot)
+
+      partial += 1
+      remove_index = code.find_index(slot)
+      code[remove_index] = 'x'
+      guess[index] = 'x'
+    end
+    partial
+  end
+end
+
 class Mastermind
+
+  include AccuracyFinder
+
   attr_reader :numbers, :slots
 
   def initialize(numbers)
@@ -34,21 +84,23 @@ class Mastermind
   end
 
   def play_cpu
-    code = make_code_manual
+    cpu = CpuPlayer.new(slots, numbers)
+    # code = make_code_manual
+    code = [1, 1, 3, 5]
     p code
     12.times do
-      guess = make_code
+      guess = cpu.cpu_guess
       p guess
-      submit_guess(guess, code)
+      cpu.bad_guess(submit_guess(guess, code))
     end
     loose
   end
 
   # GAME LOGIC HANDLING
 
-  private
+  # private
 
-  def submit_guess (guess, code)
+  def submit_guess(guess, code)
     if guess == code
       win
     else
@@ -83,67 +135,6 @@ class Mastermind
     end
   end
 
-  def get_accuracy(guess, code)
-    temp_code = code.clone
-    temp_guess = guess.clone
-    accuracy = []
-
-    get_exact(temp_guess, temp_code).times { accuracy.append('E') }
-    get_partial(temp_guess, temp_code).times { accuracy.append('P') }
-
-    accuracy
-  end
-
-  def print_accuracy(guess, code)
-    get_accuracy(guess, code).each do |result|
-      print "#{result} "
-    end
-    puts
-  end
-
-  def get_exact(guess, code)
-    exact = 0
-    guess.each_with_index do |slot, index|
-      next unless slot == code[index]
-
-      exact += 1
-      guess[index] = '-'
-      code[index] = '-'
-    end
-    exact
-  end
-
-  def get_partial(guess, code)
-    partial = 0
-    guess.each_with_index do |slot, index|
-      next unless slot != '-' && code.include?(slot)
-
-      partial += 1
-      remove_index = code.find_index(slot)
-      code[remove_index] = 'x'
-      guess[index] = 'x'
-    end
-    partial
-  end
-
-  # CPU PLAYER HANDLING
-
-  def cpu_guess(slots, numbers)
-    s_set = make_code_set(slots, numbers)
-
-  end
-
-  def make_code_set(slots, numbers)
-    set = []
-    numbers.times { |n| set.append(n) }
-    set.repeated_permutation(slots).to_a
-  end
-
-  def remove_same_response(guess, s_set)
-    # Needs to remove everything that would elicit the same response if he guess was the code
-
-  end
-
   # GAME END HANDLING
 
   def win
@@ -164,5 +155,63 @@ class Mastermind
   end
 end
 
-mastermind = Mastermind.new(4, 5)
-mastermind.start
+class CpuPlayer
+  # Makes a CPU player object that uses the Knuth codebreaking model
+
+  include AccuracyFinder
+
+  def initialize(slots, numbers)
+    @slots = slots
+    @numbers = numbers
+    @code_set = make_code_set(slots, numbers)
+    @s_set = code_set.clone
+    @guess = [1, 1, 2, 2]
+  end
+
+  attr_accessor :code_set, :s_set
+  attr_reader :slots, :numbers, :guess
+
+  def cpu_guess
+    if guess == [1, 1, 2, 2]
+      first_guess
+    else
+      next_guess
+    end
+  end
+
+  def first_guess
+    code_set.delete(guess)
+  end
+
+  def next_guess(accuracy)
+    s_set = remove_same_response(guess, accuracy)
+    guess = minimax
+    code_set.delete(guess)
+  end
+
+  def make_code_set(slots, numbers)
+    set = []
+    numbers.times { |n| set.append(n) }
+    set.repeated_permutation(slots).to_a
+  end
+
+  def remove_same_response(guess, accuracy)
+    # Needs to remove everything that would not elicit the same response if he guess was the code
+    s_set.select do |code|
+      get_accuracy(code, guess) == accuracy
+    end
+  end
+
+  def minimax
+    combinations = ['E', 'P'].repeated_permutation(slots).to_a # Create the possible response combinations
+    code_set.min_by do |code|
+      combinations.max_by do |accuracy|
+        remove_same_response(code, accuracy).length #find the maximum amount of remaining guesses for each combo
+      end
+    end
+  end
+end
+
+
+mastermind = Mastermind.new(5)
+mastermind.play_cpu
