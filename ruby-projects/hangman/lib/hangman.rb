a1d9b185f225e95fdaf955343ce912d8c79e5322
TJancenkovas <t.jancenkovas@gmail.com>
2023-06-22 12:19:02 +0300

    Finish hangman

--- ruby-projects/hangman/lib/hangman.rb
+++ ruby-projects/hangman/lib/hangman.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 class Hangman
   # Hangman game object
   #   Stores : secret_word, word_guess, remaining_letters, guess_count, max_guesses
@@ -10,8 +12,8 @@ class Hangman
   #   Run check_user guess (modifies word_guess)
   #   Restart
 
-  def initialize(filename)
-    @filename = filename
+  def initialize(word_list_file)
+    @word_list_file = word_list_file
     @secret_word = []
     @guess_count = 0
     @max_guesses = 10
@@ -19,16 +21,11 @@ class Hangman
     @remaining_letters = ('a'..'z').to_a
   end
 
-  attr_reader :filename, :max_guesses
+  attr_reader :word_list_file, :max_guesses
   attr_accessor :secret_word, :guess_count, :word_guess, :remaining_letters
 
   def start
-    puts 'Load game? (Y/N)'
-    if gets.chomp.downcase == 'y'
-      load_game
-    else
-      self.secret_word = read_word
-    end
+    self.secret_word = read_word
     play
   end
 
@@ -64,6 +61,7 @@ class Hangman
       puts 'Please enter a single letter'
     end
     remaining_letters.delete(guess)
+    guess
   end
 
   def check_guess(guess)
@@ -76,7 +74,7 @@ class Hangman
   end
 
   def read_word
-    word_list = File.open(filename, 'r').readlines
+    word_list = File.open(word_list_file, 'r').readlines
 
     word = word_list.sample.chomp
     word = word_list.sample.chomp while word.length > 12 || word.length < 5
@@ -86,41 +84,91 @@ class Hangman
     word.split('')
   end
 
-  def load_game
-    puts 'laod'
-  end
-
   def save_game
-    puts 'saved'
+    puts 'Enter savegame name:'
+    filename = gets.chomp
+    File.open("#{filename}.json", 'w') do |file|
+      file.write(to_json)
+    end
+    puts 'Game saved'
   end
 
   def win
-    puts "You win!"
+    puts 'You win!'
     retry?
   end
 
   def loose
-    puts "You loose ;w;"
+    puts 'You loose ;w;'
+    puts "The word was #{secret_word}"
     retry?
   end
 
   def retry?
-    puts "Try again? (Y/N)"
-    if gets.chomp.downcase == "y"
+    puts 'Try again? (Y/N)'
+    if gets.chomp.downcase == 'y'
       self.guess_count = 0
       self.remaining_letters = ('a'..'z').to_a
-      play
+      start
     else
       exit
     end
+  end
+
+  def to_json(*a)
+    {
+      'json_class' => self.class.name,
+      'data' =>
+      {
+        :word_list_file => word_list_file,
+        :secret_word => secret_word,
+        :guess_count => guess_count,
+        :max_guesses => max_guesses,
+        :remaining_letters => remaining_letters,
+        :word_guess => word_guess
+      }
+    }.to_json(*a)
+  end
+
+  def self.json_create(obj)
+    hangman = new(obj['data']['word_list_file'])
+    hangman.secret_word = obj['data']['secret_word']
+    hangman.guess_count = obj['data']['guess_count']
+    hangman.remaining_letters = obj['data']['remaining_letters']
+    hangman.word_guess = obj['data']['word_guess']
+    hangman
   end
 
   def letter?(char)
     code = char.downcase.ord
     code >= 97 && code <= 122
   end
+
+  def to_s
+    'test'
+  end
 end
 
+def start_game(game_obj)
+  puts 'Load game? (Y/N)'
+  if gets.chomp.downcase == 'y'
+    game = load_game(game_obj)
+  else
+    game = game_obj.new('google-10000-english-no-swears.txt')
+  end
+  game.start
+end
 
-game = Hangman.new('google-10000-english-no-swears.txt')
-puts game.start
+def load_game(game_obj)
+  puts 'Available saves:'
+  Dir['./*.json'].entries.each { |name| puts name.chomp('.json') }
+
+  puts 'Enter savegame to load:'
+  filename = gets.chomp
+  puts "#{filename}.json"
+
+  savedata = File.read("#{filename}.json")
+  game_obj.json_create(JSON.parse(savedata))
+end
+
+start_game(Hangman)
